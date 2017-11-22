15834f69aaf386aa962b0b06da7f659ac292fcf5
abner <abner009007@users.noreply.github.com>
2017-11-22 16:25:39 +0800

    添加方法
    添加方法

--- SwiftScreenImage/ViewController.swift
+++ SwiftScreenImage/ViewController.swift
@@ -25,7 +25,7 @@ class ViewController: UIViewController {
     }
     
     
-    //获取屏幕截图
+    //获取屏幕截图 需要关心一下最上层是谁   
     func screenSnapshot(save: Bool) -> UIImage? {
         
         guard let window = UIApplication.shared.keyWindow else { return nil }
@@ -42,6 +42,26 @@ class ViewController: UIViewController {
         if save { UIImageWriteToSavedPhotosAlbum(image!, self, nil, nil) }
         
         return image
+    }
+    //不用关心最上层是那个view   直接截取的是最上层的
+    func screenSnapshot2(save: Bool) -> UIImage? {
+        let windown = UIApplication.shared.delegate?.window
+        
+        // 01 开启图片上下文
+        UIGraphicsBeginImageContextWithOptions((windown?!.bounds.size)!, true, UIScreen.main.scale)
+        //afterScreenUpdates如果是true, 则是addSubview之后,  如果是false,则是addSubview之前
+        windown??.drawHierarchy(in: (windown??.bounds)!, afterScreenUpdates: true)
+        // 获取图片
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        
+        // 关闭图片上下文
+        UIGraphicsEndImageContext()
+        
+        //  将图片存储到本地
+        if save { UIImageWriteToSavedPhotosAlbum(image!, self, nil, nil) }
+        
+        return image
+        
     }
     
     override func didReceiveMemoryWarning() {
